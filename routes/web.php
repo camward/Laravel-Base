1f722431c3903e3e9ee253486600ee9bfc163a6f
alex <camward@yandex.ru>
2017-02-21 13:56:19 +0400

    Route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,51 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/page', function () {
+    // echo "<pre>";
+    // print_r($_ENV);
+    // echo "</pre>";
+    // echo config('app.locale'); // название_файла_конфига.настройка
+    // echo Config::get('app.locale');
+    echo env('APP_ENV'); // название ключа
+});
+
+// для комбинированных запросов
+Route::match(['get', 'post', 'put'], '/match', function () {
+    echo "match";
+});
+
+// для всех типов запросов
+Route::any('/any', function () {
+    echo "any";
+});
+
+// использование параметров
+Route::get('/params/{id}', function ($id = null) { // {id} - параметр обязательно должен быть. {id?} - параметр НЕобязателен
+    echo "id = " . $id;
+})->where('id', '[0-9]+'); // регулярное выражение для фильтрации параметров
+// если переменных несколько, то передавать через массив: where(['id'=>'[0-9]+', 'category_name'=>'[A-Za-z]+'])
+
+// группы маршрутов
+Route::group(['prefix'=>'admin'], function() {
+    // страницы будут доступны по адресу /admin/page/...
+
+    Route::get('/page/create', function () {
+        return redirect()->route('article', array('id'=>25));
+    });
+    Route::get('/page/update', function () {
+        echo "update";
+    });
+    Route::get('/page/view', function () {
+        echo "view";
+    });
+    Route::get('/page/delete', function () {
+        echo "delete";
+    });
+});
+
+// сформируем ссылку на route, к которой можно будет обращаться по redirect()->route('article', array('id'=>25))
+Route::get('article/{id}', ['as'=>'article', function ($id){
+    echo $id;
+}]);
